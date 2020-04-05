2087082d8a5ff5afe94f7148aec5a8beeb4fa270
Dariusz Ochal <d.ochal@ams.com.pl>
2020-04-05 19:22:05 +0200

    - Initial commit

---
+++ server.js
@@ -0,0 +1,31 @@
+/**
+ * Module dependencies.
+ */
+const express = require('express');
+const session = require('express-session');
+const bodyParser = require('body-parser');
+
+/**
+ * Load environment variables from .env file, where API keys and passwords are configured.
+ */
+dotenv.config({ path: '.env.example' });
+
+/**
+ * Controllers (route handlers).
+ */
+const homeController = require('./controllers/home');
+const userController = require('./controllers/user');
+const apiController = require('./controllers/api');
+const contactController = require('./controllers/contact');
+
+/**
+ * Create Express server.
+ */
+const app = express();
+
+app.listen(app.get('port'), () => {
+    console.log('%s App is running at http://localhost:%d in %s mode', chalk.green('✓'), app.get('port'), app.get('env'));
+    console.log('  Press CTRL-C to stop\n');
+});
+  
+module.exports = app;
